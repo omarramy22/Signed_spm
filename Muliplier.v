1cafeeb06d010f8276f4f4bb416cf0730cf6ba89
omarosamaa20 <omarosamaa@aucegypt.edu>
2025-05-19 23:05:57 +0300

    Update multiplier to ensure signed multiplication

--- Muliplier.v
+++ Muliplier.v
@@ -18,12 +18,13 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-module serial_parallel_multiplier(input clk, rst, start, input [7:0] multiplicand, multiplier, output reg [15:0] product, output reg done);
-    reg [7:0] A;
-    reg [7:0] B;
-    reg [15:0] P;
+module serial_parallel_multiplier(input clk, rst, start, input signed [7:0] multiplicand, multiplier, output reg signed [15:0] product,output reg done);
+    reg signed [7:0] A;
+    reg signed [7:0] B;
+    reg signed [15:0] P;
     reg [3:0] count;
     reg active;
+    reg B_sign;
     
     always @(posedge clk or posedge rst) begin
         if (rst) begin
@@ -34,9 +35,11 @@ module serial_parallel_multiplier(input clk, rst, start, input [7:0] multiplican
             count <= 0;
             active <= 0;
             done <= 0;
+            B_sign <= 0;
         end else if (start && !active) begin
-            A <= multiplicand;
-            B <= multiplier;
+            B_sign <= multiplier[7];
+            A <= multiplicand[7] ? -multiplicand : multiplicand;
+            B <= multiplier[7] ? -multiplier : multiplier;
             P <= 0;
             count <= 0;
             active <= 1;
@@ -45,13 +48,18 @@ module serial_parallel_multiplier(input clk, rst, start, input [7:0] multiplican
             if (B[0]) begin
                 P <= P + (A << count);
             end
-
             B <= B >> 1;
             
             count <= count + 1;
             
             if (count == 7) begin
-                product <= B[0] ? P + (A << count) : P;
+                P <= B[0] ? P + (A << count) : P;
+                
+                if (multiplicand[7] ^ B_sign) begin
+                    product <= -(B[0] ? P + (A << count) : P);
+                end else begin
+                    product <= B[0] ? P + (A << count) : P;
+                end
                 active <= 0;
                 done <= 1;
             end
